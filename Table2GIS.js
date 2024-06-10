c157d108c2f01d6a6158395ebea3fb987e31d4f2
Patrick <patrick@Beatrices-MBP.lan>
2024-06-10 10:33:12 -0400

    Commented Table 2

--- Table2GIS.js
+++ Table2GIS.js
@@ -6,63 +6,68 @@ function calculateTable2GIS(SE, TU, PE, INC, YEARS, IYEARS = 10, AGE, DATE = "20
   // Cannot handle retroactive rate calculations
   // Cannot handle International Operations calculations
   //////////////////////////////////////////////////////////////////
-  // Supplement Equivalent - Single (SE) 
-  // Top-Up - Single (TU)
+  // Supplement Equivalent - Married (SE)
+  // This is the maximum GIS rate for a pensioner married to another pensioner, not including the top-up         as provided for under s.12 of the OAS Act
+  //
+  // Top-Up - Married (TU)
+  // This is the maximum Top-Up rate for a pensioner married to another pensioner as provided for under ss. 12.1(2) of the OAS Act.
+  //
   // Pension Equivalent (PE)
-  // Single Income (INC)
+  // This is the maximum OAS rate, i.e. 40/40ths
+  //
+  // Joint Income (INC)
+  // Each member of a couple has their individual income determined as though they were single, i.e. their own net income under the Income Tax Act, subject to allowable deductions under “income” in s.2 of the OAS Act, and then their single incomes added together.
+  //
   // IO Years (IYEARS)
+  //This is the person’s number of full years in Canada at the time of the payment months being calculated.
   // Date to be used in future iterations for retroactive calculations
-
+  //
   // Calculate Special Qualifying Factor (SQF)
+  //This fraction is the number of years of Canadian residence over 10 to a maximum of 10 years. This number is usually 1 unless the person isa recent immigrant. Definition of SQF found in s.2 of OAS Act.
   const SQF = Math.min(1, IYEARS/10);
-
+  
   // Calculate Full Monthly Pension
+  // This is the maximum OAS Rate, i.e. 40/40ths. If person over 75, max OAS rate is 10% higher than age 65 rate per s.7 of OAS Act.
   const FMP = AGE >= 75 ? PE * 1.1 : PE
-
+  
   // Calculate Years Based OAS
+  //This is the actual OAS rate the person is entitled to, whether a full pension or a partial rate determined under s.3 of the OAS Act.
   const YOAS = (Math.min(YEARS, 40)/40) * FMP
-
+  
   // // Calculate Monthly Joint Income (MJI) and MJI over $4000 (MJI4K)
-  const MBIA = Math.floor(INC / 48);
-  const MBI4K = Math.floor(min0(INC - 4000) / 48);
-
-  // Calculate Base Summplement (BS)
-  const BS = min0(((SE + PE - YOAS) * SQF) - MBIA)
-
+  // This part of the GIS calculation formula takes the couple’s joint income divided by 24 to get the monthly base income and then divides it by 2. This is the 50% reduction rate, meaning the max GIS is reduced by $1 for every $2 of joint income.
+  const MBIA = Math.floor((INC / 24)/ 2);
+  
+  //This is the income part of the top-up formula where the first $4000 is subtracted from the couple’s joint income, which is then divided by 24 for the monthly base income and then divided by 4. Again, related to reductions rates.
+  const MBI4K = Math.floor(min0((INC - 4000) / 24) /4) ;
+  
+  // Calculate Base Supplement (BS)
+  //This is the GIS formula set out under subsection 12(5) of the OAS Act
+  //(Max GIS rate (without top-up) + Max OAS rate – pensioners actual OAS monthly rate) x Special Qualifying Factor – pensioner’s monthly base income /2
+  const BS = min0((SE + FMP - YOAS) * SQF) - MBIA
+  
   // Calculate Modified Top-Up (MTU)
+  // This is the GIS top-up formula set out under subsection 12.1(2) of the OAS Act
+  // Max married top-up rate x Special Qualifying Factor less the income reduction
   const MTU = min0((TU * SQF) - MBI4K)
-
-  const GIS = addOAS ? BS + MTU + FMP : BS + MTU;
-
-
+ 
+  // By default, this only returns GIS. However we can add the FMP for testing purposes
+  const GIS = addOAS ? BS + MTU + FMP : BS + MTU +FMP - PE;
   return GIS.toFixed(2);
-}
-
-// Example usage
-// Supplement Equivalent (SE)
-const SE = 594.59; // Example values, replace with actual values
-// Top-Up (TU)
-const TU = 46.76;
-// Pension Equivalent (PE)
-const PE = 713.34;
-// OAS and International Years
-const YEARS = 40
-// Client age
-const AGE = 65
-
-
-console.log("=======================================");
-console.log(0, calculateTable2GIS(SE, TU, PE, 0, YEARS, YEARS, AGE, "2024-04-01", true), ":", 1354.69, ":", 1426.02);
-console.log(100, calculateTable2GIS(SE, TU, PE, 100, YEARS, YEARS, AGE, "2024-04-01", true), ":", 1352.69, ":", 1424.02);
-console.log(1000, calculateTable2GIS(SE, TU, PE, 1000, YEARS, YEARS, AGE, "2024-04-01", true), ":", 1334.69, ":", 1406.02);
-console.log(2500, calculateTable2GIS(SE, TU, PE, 2500, YEARS, YEARS, AGE, "2024-04-01", true), ":", 1302.69, ":", 1374.02);
-console.log(5000, calculateTable2GIS(SE, TU, PE, 5000, YEARS, YEARS, AGE, "2024-04-01", true), ":", 1240.69, ":", 1312.02);
-console.log(8000, calculateTable2GIS(SE, TU, PE, 8000, YEARS, YEARS, AGE, "2024-04-01", true), ":", 1147.69, ":", 1219.02);
-console.log(10000, calculateTable2GIS(SE, TU, PE, 10000, YEARS, YEARS, AGE, "2024-04-01", true), ":", 1099.93, ":", 1171.26);
-console.log(12000, calculateTable2GIS(SE, TU, PE, 12000, YEARS, YEARS, AGE, "2024-04-01", true), ":", 1057.93, ":", 1129.26);
-console.log(15000, calculateTable2GIS(SE, TU, PE, 15000, YEARS, YEARS, AGE, "2024-04-01", true), ":", 995.93, ":", 1067.26);
-console.log(20000, calculateTable2GIS(SE, TU, PE, 20000, YEARS, YEARS, AGE, "2024-04-01", true), ":", 891.93, ":", 963.26);
-console.log(25000, calculateTable2GIS(SE, TU, PE, 25000, YEARS, YEARS, AGE, "2024-04-01", true), ":", 787.93, ":", 859.26);
-console.log(30000, calculateTable2GIS(SE, TU, PE, 30000, YEARS, YEARS, AGE, "2024-04-01", true), ":", 713.34, ":", 713.34*1.1);
+ }
+ 
+ // Example usage
+ // Supplement Equivalent (SE)
+ const SE = 594.59; // Example values, replace with actual values
+ // Top-Up (TU)
+ const TU = 46.76;
+ // Pension Equivalent (PE)
+ const PE = 713.34;
+ // OAS and International Years
+ const YEARS = 40
+ // Client age
+ const AGE = 75
+ // Add OAS for debugging
+ const ADDOAS = true
 
 module.exports = { calculateTable2GIS };
