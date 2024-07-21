88c525691aaba857bbcef43c6aa160e9e5b44fe4
Patrick <patrick@Beatrices-MBP.lan>
2024-07-21 16:07:59 -0400

    Table 4 comments

--- package/Table4ALW.js
+++ package/Table4ALW.js
@@ -1,50 +1,62 @@
 const { roundUp, roundDown, min0 } = require("./utils.js");
 
 function calculateTable4ALW(SE, TU, PE, INC, IYEARS = 10, DATE = "2024-04-01") {
-  //////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
   // This function is accurate for rates from 04-01-2024 and beyond
   // Cannot handle retroactive rate calculations
   // Cannot handle International Operations calculations
   //////////////////////////////////////////////////////////////////
   // Supplement Equivalent (SE)
+	//This is the maximum GIS rate for a pensioner who is married to another pensioner, not including the top-up, as provided for under s.22 of the OAS Act.
   // Top-Up (TU)
+	//This is the maximum Top-Uprate for an Allowance recipient as provided for under ss 22.1(2) of the OAS Act.
   // Pension Equivalent (PE)
+	//This is the maximum OAS rate, i.e. 40/40th, at the +65 rate only.
   // Joint Income (INC)
+	//Each member of a couple has their individual income determined as though they were single, i.e. theirown net income under the Income Tax Act, subject to allowable deductions under "incomein s.2 ofthe OAS Act, and then their single incomes add together.
  // IO Years (IYEARS)
+	//This is the person's number of full years in Canada at the time of the payment months being calculated.
   // Date to be used in future iterations for retroactive calculations
 
   // Calculate Special Qualifying Factor (SQF)
+  //This fraction isthe number of years of Canadian residence over 10 to a maximum of 10years. This number is usually 1 unless the person is a recent immigrant. Definition of SQF found in s.2 of the OAS Act.
   const SQF = Math.min(1, IYEARS/10);
 
   // Calculate Monthly Joint Income (MJI) and MJI over $4000 (MJI4K)
+  //This first part of the ALW calculation formula takes the couple's joint income and divides it by 12 to get the monthly amount.
   const MJI = min0(INC / 12);
+
+  //This is the income part of the top-up formula where the first $4000 is substractd from the couple's joint income, which is then divided by 24 for the monthly base income and then divided by 4. This is related to the reduction rates.
   const MJI4K = min0((INC - 4000) / 12);
 
   // Step 3: Apply SQF to System Values
-  const QSE = SQF * SE;
-  const QPE = SQF * PE;
-  const QTU = SQF * TU;
-  const QRPE = roundUp(PE, 3) * SQF;
-  const PRODUCT = (4 / 3) * QRPE;
+  const QSE = SQF * SE; //This is the maximum supplement rate (i.e. married rate, pensioner married to pensioner) not including top-up multiplied by special qualifying factor
+  const QPE = SQF * PE; //This is the pension equivalent, which is the maximum +65 OAS pension rate, multiplied by the special qualifying factor.
+  const QTU = SQF * TU; 	//This is the maximum top-up, at the married rate, multiplied by the special qualifying factor.
+  const QRPE = roundUp(PE, 3) * SQF; //This is the rounded pension equivalent, i.e.max OAS rate (40/40th) rounded to the next higher multiple of 3, if not already a multiple of 3, and then mulitiplied by the special qualifying factor.
+  const PRODUCT = (4 / 3) * QRPE; //To determine which ALW formula to use to calculate the benefit, you must first determine the "product". The "product" is 4/3 of the rounded pension equivalent multipled by the special qualifying factor.
 
   // Calculate 3/4 Monthly Joint Income (MJI3/4)
-  const MJI34 = min0((3 / 4) * roundDown(MJI, 4));
+  const MJI34 = min0((3 / 4) * roundDown(MJI, 4)); ////For the ALW formula under paragraph 22(3)(b), for the income portion of the formula, the maximum benefit rate is reduced by 3/4 of the monthly joint income, which is then rounded down to the lower multiple of 4 where it is not already a multiple of 4.
 
   // Calculate Residual Joint Income (RJI)
+  //This formula is A - B, where A is the monthly joint income of pensioner and spouse/common-law partner in the current payment period and B is four-thirds of the rounded pension equivalent, multiplied by the special qualifying factor and rounded to the next higher multiple of $4 where it is not already a multiple of $4. This formula is found in the definitions section of s.22 of the OAS Act.
   const RJI = MJI - roundUp((4 / 3) * QRPE, 4);
 
   // Calculate Income-Adjusted Top-Up (IATU)
+  //This formula takes the maximum married top-up rate, multiplies it by the special qualifying factor and then subtracts the income reduction, which is 1/24th of the couple's joint income for the base calendar year in excess of $4000 and rounded to the next lower multiple of 4 if not already a multiple of 4, and then this amount is divided by 4.
   const IATU = min0(QTU - roundDown(MJI4K / 2, 4) / 4);
 
   let ALW;
 
   // Step 7: Calculate Total Allowance (ALW) based on INC
+  //The ALW calculation differs depending on level of income
   if (INC === 0) {
-    ALW = QPE + QSE + QTU;
+    ALW = QPE + QSE + QTU; //If joint income is NIL, then the maximum ALW rate is payable (a combination of the maximum pension equivalent and maximum supplement equivalent), plus the maximum top-up rate.
   } else if (MJI <= PRODUCT) {
-    ALW = QSE + (QPE - MJI34) + IATU;
+    ALW = QSE + (QPE - MJI34) + IATU; //If the couple's joint income is greater than 0 and less than or equal to the "product", the ALW formula reduces the pension equivalent portion based on income, where the pension equivalent is reduced by 3/4 of the monthly joint income that is rounded to the next lower multiple of 4 where not already a multiple of 4.
   } else {
-    ALW = QSE - roundDown(RJI, 4) / 4 + IATU;
+    ALW = QSE - roundDown(RJI, 4) / 4 + IATU; //If the couple's joint income is greater than the "product", the ALW formula starts to reduce the supplement equivalent portion as well. The supplement equivalent is reduced by the residual joint income of the couple, rounded to the next lower multiple of 4 where not already a multiple of 4 and then this amount is divided by 4.
   }
 
   return ALW.toFixed(2);
@@ -60,21 +72,6 @@ const PE = 713.34;
 // Joint Income (INC)
 const INC = 11000;
 
-
-// console.log("=======================================");
-// console.log(0, calculateTable4ALW(SE, TU, PE, 0), ":", 1354.69);
-// console.log(100, calculateTable4ALW(SE, TU, PE, 100), ":", 1348.69);
-// console.log(1000, calculateTable4ALW(SE, TU, PE, 1000), ":", 1294.69);
-// console.log(2500, calculateTable4ALW(SE, TU, PE, 2500), ":", 1198.69);
-// console.log(5000, calculateTable4ALW(SE, TU, PE, 5000), ":", 1032.69);
-// console.log(8000, calculateTable4ALW(SE, TU, PE, 8000), ":", 815.69);
-// console.log(10000, calculateTable4ALW(SE, TU, PE, 10000), ":", 683.93);
-// console.log(12000, calculateTable4ALW(SE, TU, PE, 12000), ":", 582.59);
-// console.log(15000, calculateTable4ALW(SE, TU, PE, 15000), ":", 520.59);
-// console.log(17500, calculateTable4ALW(SE, TU, PE, 17500), ":", 468.59);
-// console.log(20000, calculateTable4ALW(SE, TU, PE, 20000), ":", 416.59);
-// console.log(25000, calculateTable4ALW(SE, TU, PE, 25000), ":", 312.59);
-// console.log(30000, calculateTable4ALW(SE, TU, PE, 30000), ":", 207.69);
 
 
 module.exports = { calculateTable4ALW };
--- package/Table4GIS.js
+++ package/Table4GIS.js
@@ -1,50 +1,74 @@
 const { roundUp, roundDown, min0 } = require("./utils.js");
 
 function calculateTable4GIS(SE, TU, PE, INC, YEARS, IYEARS = 10, AGE, DATE = "2024-04-01") {
-  //////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
   // This function is accurate for rates from 04-01-2024 and beyond
   // Cannot handle retroactive rate calculations
   // Cannot handle International Operations calculations
   //////////////////////////////////////////////////////////////////
-  // Supplement Equivalent (SE)
-  // Top-Up (TU)
+  // Supplement Equivalent (SE)- Married to an OAS Allowance recipient
+	// This is the maximum GIS rate for a pension who is married to an OAS Allowance Recipient, not including the top-up, as provided for under s.22 of the OAS Act. It is the same as the married rate for pensioners married to another OAS pensioner under Table 2.
+  // Top-Up (TU)- Married to an OAS Allowance recipient
+	// This is the maximum Top-Up rate for a pensioner married to an OAS Allowance Recipient as provided for under ss22.1(1) of the OAS Act.
   // Pension Equivalent (PE)
+	//This is the maximum OAS rate. i.e. 40/40ths
   // Joint Income (INC)
+	//Each member of a couple has their individual income determined as though they were single, i.e. their own net income under the Income Tax Act, subject to allowable deductions under "income" in s.2 of the OAS Act, and then their single incomes added together.
   // IO Years (YEARS)
+	// This is the person's number of full years in Canada at the time of the payment months being calculated.
   // Date to be used in future iterations for retroactive calculations
 
   // Calculate Full Monthly Pension
+	//This is the maximum OAS rate, i.e.40/40ths. If person over 75, max OAS rate is 10% higher than age 65 rate per s.7 of OAS Act.
   const FMP = AGE >= 75 ? PE * 1.1 : PE
 
-  // Calculate Rounded Pension Equivalent
+ // Calculate Rounded Pension Equivalent
+	//This is the maximum OAS rate, i.e. 40/40th rounded to the next higher multiple of $3 when not already a multiple of 3.
   const RPE = roundUp(PE, 3)
   
   // Calculate Years Based OAS
+	//This is the actual OAS rate the person is entitled to, whether a full pension or a partial rate determined under s.3 of the OAS Act.
   const YOAS = (Math.min(YEARS, 40)/40) * FMP
 
   // Calculate Special Qualifying Factor (SQF)
+	//This fraction is the number of years of Canadian residence over 10 to a maximum of 10 years. This number is usually 1 unless the person is a recent immigrant. Definition of SQF found in s.2 of the OAS Act.
   const SQF = Math.min(1, IYEARS/10);
 
   // Calculate Monthly Joint Income (MJI) and MJI over $4000 (MJI4K)
+	//This part of the GIS calculation formula takes the couple's joint income and divides it by 12 to get the monthly amount.
   const MJI = min0(INC / 12);
+  	//This is the income part of the top-up formula where the first $4000 is subtracted from the couple's joint income, which is then divided by 24 for the monthly base income and then divided by 4. This is related to the reduction rates. 
   const MJI4K = roundDown(min0(INC - 4000) / 24, 4);
 
   // const QPE = SQF * PE;
+  //This is the maximum OAS rate, i.e. 40/40ths multiplied by the Special Qualifying Factor
   const QTU = SQF * TU;
+  //This is the manximum Top-Up rate multiplied by the Special Qualifying Factor.
   const QRPE = RPE * SQF;
 
-  // Calculate Residual Joint Income (RJI)
+  // This is the rounded pension equivalent, i.e max OAS rate (40/40th) rounded to the next higher multiple of 3 if not already a mulitple of 3 and then multiplied by the Special Qualifying Factor.
+
+ // Calculate Residual Joint Income (RJI)
+	//This formula is A - B, where A is the monthly joint income of pensioner and spouse/common-law partner in the current payment period and B is four-thirds of the rounded pension equivalent, multiplied by the special qualifying factor and rounded to the next higher mulitple of $4 where it is not already a multiple of $4. This formula is found in the definitions section of s.22 of the OAS Act. the
   const RJI = min0(MJI - roundUp((4 / 3) * QRPE, 4));
+
+  	//The result of the A-B formula for the RJI is then rounded to the next lower multiple of 4 where not already a multiple of 4 and then that result is divided by 4.  This last part is the income reduction factor, i.e where every $2 of your individual income reduces your GIS by $1
   const RJIA = roundDown(RJI, 4) / 4 // D/4
 
-  // Calculate Base Summplement (BS)
+  // Calculate Base Sumpplement (BS)
+	//This is the GIS formula set out under subsection 22(2) of the OAS Act.
+	//(Max GIS rate (without top-up)+ Max OAS rate - pensioners actual OAS monthly rate) x special qualifying factor -  (residual joint income, rounded and divided by 4))
   const BS = ((SE + FMP - YOAS) * SQF) - RJIA
 
   // Calculate Modified Top-Up (MTU)
+	//This is the GIS top-up formula set out under subsection 22.1(1) of the OAS Act.
+	//Max married top-up rate x Special Qualifying Factor less the income reduction.
   const MTU = min0(QTU - MJI4K / 4)
 
   // Calculate Income-Adjusted Top-Up (IATU)
   const GIS = BS + MTU + YOAS
+
+  	//This is the total OAS benefit amount: the person's base supplement amount + their top-up amount based on their level ofincome and added to their OAS pension rate based on their years of residence in Canada.
 
   return GIS.toFixed(2);
 }
